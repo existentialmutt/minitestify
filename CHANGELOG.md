5c6080c0f2c8be832df625d181f80e2ee3d86625
Rafe Rosen <rafe@existentialmutt.com>
2022-11-22 19:08:49 -0500

    initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+## [Unreleased]
+
+## [0.1.0] - 2022-11-22
+
+- Initial release
---
+++ lib/minitestify.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "minitestify/version"
+
+module Minitestify
+  class Error < StandardError; end
+  # Your code goes here...
+end
